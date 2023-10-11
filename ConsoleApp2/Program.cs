85487d425092fcde6124c69cf21337d3ca9f645e
12 <12@DESKTOP-NK7HEP0>
2023-10-11 15:04:44 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите количество жителей в государстве");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите площадь территории государства");
+double b = Convert.ToDouble(Console.ReadLine());
+double q = a / b; //Формула плотности населения
+Console.WriteLine($"Плотность населения = {q}");
